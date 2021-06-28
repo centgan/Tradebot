59b545fbf7d7658f36f06ad9063159be77f4a27a
Gancent <83138403+Gancent@users.noreply.github.com>
2021-06-28 08:03:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,48 +6,43 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-import json
-import oandapyV20
-from oandapyV20 import API
-from oandapyV20.exceptions import V20Error
-import oandapyV20.endpoints.accounts as accounts
-import oandapyV20.endpoints.trades as trades
-import oandapyV20.endpoints.pricing as pricing
-from oandapyV20.contrib.factories import InstrumentsCandlesFactory
-import datetime
-from datetime import datetime, timedelta
+import Connect
 import Trend
 import Other
 import Order
-
-
+import json
+import datetime
 
 # can optimize further by saying if current price is not close to previous high or low
 # then check every 30 seconds or min instead of checking constantly
 
+
+pair_list = ["GBP_AUD", "GBP_USD", "NAS100_USD"]
+
 while True:
-    Trend.dumphist("GBP_AUD", "M30")
-    Trend.dumpcur("GBP_AUD")
-    orderlist = Order.buyorsell()
-    Order.watch(orderlist)
-    print(orderlist)
+    day = datetime.datetime.today().weekday()
+    time = datetime.datetime.now().strftime("%H")
+    if day == 6 and int(time) >= 17:
+        for i in pair_list:
+            Trend.dumphist(i, "M30")
+            Trend.overall(i)
+    elif 0 >= day >= 3:
+        for i in pair_list:
+            Trend.dumphist(i, "M30")
+            Trend.overall(i)
+            Order.buyorsellnew(i)
+            Order.watchnew(i)
+            Connect.db()
+            Connect.changes()
+    elif day == 4 and int(time) <= 17:
+        for i in pair_list:
+            Trend.dumphist(i, "M30")
+            Trend.overall(i)
+            Order.buyorsellnew(i)
+            Order.watchnew(i)
+            Connect.db()
+            Connect.changes()
+    elif day == 5:
+        pass
 
-#
-# Trend.dumphist("GBP_AUD", "M30")
-# data = Other.fetchjson("his")
-# result = Trend.overall()
-# highandlow = result[2]
-#
-# curfile = Other.fetchjson("cur")
-# bid = float(curfile["prices"][0]["bids"][0]["price"])
-# asks = float(curfile["prices"][0]["asks"][0]["price"])
-# prehigh = highandlow[-1][0]
-# prelow = highandlow[-1][1]
-#
-# print(prehigh)
-# print(bid)
-# Order.buyorsell()
-
-#print(data["prices"][0]["bids"])
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
